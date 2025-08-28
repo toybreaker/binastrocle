23ab0583727107514cadfa046ce04b6822190fcb
toybreaker <toybreaker@users.noreply.github.com>
2025-08-28 18:47:02 +0200

    cleaned up + proper config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,45 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  devOptions: {
+    sourceMap: true, // <-- ENABLE this to show data-astro-source-file infos !
+  },
+  output: "static",
+  image: {
+    responsiveStyles: true,
+    layout: "constrained",
+  },
+  build: {
+    format: "directory",
+    inlineStylesheets: "always",
+  },
+  prefetch: {
+    // defaultStrategy: "viewport",
+    prefetchAll: true,
+  },
+  collections: {
+    myDataCollection: {
+      directory: "src/content",
+      routeByDefault: true,
+    },
+  },
+  trailingSlash: "never",
+  root: "./",
+  site: "https://binocle.it",
+  outDir: "./dist",
   devToolbar: {
     enabled: false,
   },
+  vite: {
+      resolve: {
+        alias: {
+          "@utils": "/src/utils",
+        },
+      },
+      server: {
+        fs: {
+          deny: ["**/src/_lab/**", "**/_lab/**"],
+        },
+      },
+    },
 });
