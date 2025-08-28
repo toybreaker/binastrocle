c760859ed78d2d06e2cbedbdac66d84dd6e35d74
toybreaker <toybreaker@users.noreply.github.com>
2025-08-28 05:14:20 +0200

    reduced set, just 4 works

--- src/content.config.js
+++ src/content.config.js
@@ -1,3 +1,4 @@
+
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
@@ -10,16 +11,7 @@ const works = defineCollection({
     title: z.string(),
     description: z.string(),
     image: z.string(),
-    image1: z.string(),
-    image2: z.string(),
-    image3: z.string(),
-    image4: z.string(),
-    image5: z.string(),
-    image6: z.string(),
-    image7: z.string(),
-    image8: z.string(),
-    image9: z.string(),
-    image10: z.string(),
+    images: z.array(z.number()).optional(),
   }),
 });
 
--- src/content/works/housecv/index.md
+++
@@ -1,8 +0,0 @@
----
-layout: works
-hash: housecv
-project: house c v
-title: Binocle - House c v
-description: ""
-image: assets/p/housecv/housecv-01-1024.jpg
----
